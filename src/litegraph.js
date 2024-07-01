087899f9c3f1700ee64e1f87f39c85cd128a172f
Daniel Lewis <daniel.lewis@plailabs.com>
2024-06-30 21:34:21 -0600

    Fix to width and height errors in ContextMenu

--- src/litegraph.js
+++ src/litegraph.js
@@ -13696,8 +13696,6 @@ LGraphNode.prototype.executeAction = function(action)
         if (options.className) {
             root.className += " " + options.className;
         }
-        root.style.minWidth = 100;
-        root.style.minHeight = 100;
         root.style.pointerEvents = "none";
         setTimeout(function() {
             root.style.pointerEvents = "auto";
