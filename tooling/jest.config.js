03942501e1b2d374a8b852182feec864c861047d
Daniel Lewis <daniel.lewis@plailabs.com>
2024-06-30 18:24:19 -0600

    Created /tooling directory and moved most tooling config files into it.

--- tooling/jest.config.js
+++ tooling/jest.config.js
@@ -23,7 +23,7 @@ module.exports = {
   // collectCoverageFrom: undefined,
 
   // The directory where Jest should output its coverage files
-  coverageDirectory: "coverage",
+  coverageDirectory: "../coverage",
 
   // An array of regexp pattern strings used to skip coverage collection
   // coveragePathIgnorePatterns: [
@@ -124,9 +124,9 @@ module.exports = {
   // rootDir: undefined,
 
   // A list of paths to directories that Jest should use to search for files in
-  // roots: [
-  //   "<rootDir>"
-  // ],
+   roots: [
+     "../"
+   ],
 
   // Allows you to use a custom runner instead of Jest's default test runner
   // runner: "jest-runner",
