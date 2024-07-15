7794195c04ff27696e4076aafb01f5c8b4b41dab
DrJKL <DrJKL0424@gmail.com>
2024-07-15 14:31:47 -0700

    Fix entry point

--- vite.config.ts
+++ vite.config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
   build: {
     outDir: "./build/dist",
     lib: {
-      entry: path.resolve(__dirname, "src/index.ts"),
+      entry: path.resolve(__dirname, "src/litegraph.js"),
       name: getPackageNameCamelCase(),
       formats,
       fileName: format => fileName[format],
