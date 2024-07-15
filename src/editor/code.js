fcc789a5a1270a45c36819e01cc538ac1add79ae
Daniel Lewis <daniel.lewis.ab@gmail.com>
2024-07-15 13:29:31 -0600

    Update to asset paths in code.js

--- src/editor/code.js
+++ src/editor/code.js
@@ -99,14 +99,14 @@ function addDemo( name, url )
 }
 
 //some examples
-addDemo("Features", "../assets/examples/features.json");
-addDemo("Benchmark", "../assets/examples/benchmark.json");
-addDemo("Subgraph", "../assets/examples/subgraph.json");
-addDemo("Audio", "../assets/examples/audio.json");
-addDemo("Audio Delay", "../assets/examples/audio_delay.json");
-addDemo("Audio Reverb", "../assets/examples/audio_reverb.json");
-addDemo("MIDI Generation", "../assets/examples/midi_generation.json");
-addDemo("Copy Paste", "../assets/examples/copypaste.json");
+addDemo("Features", "./assets/examples/features.json");
+addDemo("Benchmark", "./assets/examples/benchmark.json");
+addDemo("Subgraph", "./assets/examples/subgraph.json");
+addDemo("Audio", "./assets/examples/audio.json");
+addDemo("Audio Delay", "./assets/examples/audio_delay.json");
+addDemo("Audio Reverb", "./assets/examples/audio_reverb.json");
+addDemo("MIDI Generation", "./assets/examples/midi_generation.json");
+addDemo("Copy Paste", "./assets/examples/copypaste.json");
 addDemo("autobackup", function(){
   var data = localStorage.getItem("litegraphg demo backup");
   if(!data)
@@ -126,12 +126,12 @@ function enableWebGL()
 
   var libs = [];
   var modlibs = [
-    "/src/libs/gl-matrix-min.js",
-    "/src/libs/litegl.js",
-    "/src/nodes/gltextures.js",
-    "/src/nodes/glfx.js",
-    "/src/nodes/glshaders.js",
-    "/src/nodes/geometry.js"
+    "./src/libs/gl-matrix-min.js",
+    "./src/libs/litegl.js",
+    "./src/nodes/gltextures.js",
+    "./src/nodes/glfx.js",
+    "./src/nodes/glshaders.js",
+    "./src/nodes/geometry.js"
   ];
 
   function fetchJS()
