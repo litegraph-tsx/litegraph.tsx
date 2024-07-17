76e23524a1186481a5ac6e777181dcf74eb86496
Daniel Lewis <daniel.lewis.ab@gmail.com>
2024-07-17 16:45:53 -0600

    Mmm, now setting level can be dynamic.

--- src/core/Console.js
+++ src/core/Console.js
@@ -7,11 +7,11 @@
 export const console = new Proxy(window.console, {
   set(target, property, value) {
     if (property === 'level') {
-      target.error = (value > 0? Reflect.get(target, 'error') : () => {});
-      target.warn = (value > 1? Reflect.get(target, 'warn') : () => {});
-      target.log = (value > 2? Reflect.get(target, 'log') : () => {});
-      target.info = (value > 3? Reflect.get(target, 'info') : () => {});
-      target.debug = (value> 4? Reflect.get(target, 'debug') : () => {});
+      target.error = (value > 0? window.console.error : () => {});
+      target.warn = (value > 1? window.console.warn : () => {});
+      target.log = (value > 2? window.console.log : () => {});
+      target.info = (value > 3? window.console.info : () => {});
+      target.debug = (value> 4? window.console.debug : () => {});
       return true;
     } else {
       return Reflect.set(target, property, value);
