b296ce135c433ab4bb3c85e777e7c26fd9589963
Daniel Lewis <daniel.lewis.ab@gmail.com>
2024-07-16 22:12:01 -0600

    Tidy-up a bit in ESLint Config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,24 +7,28 @@ export default [
     "name": "litegraph.tsx/standard",
     "languageOptions": {
       "ecmaVersion": "latest",
-      "sourceType": "module"
+      "sourceType": "module",
     },
     "plugins": {
-      "@stylistic":stylistic
+      "@stylistic": stylistic,
     },
     "rules": {
-      "no-inner-declarations":0,
-      "no-prototype-builtins":0,
-      "no-redeclare":0,
-      "no-undef":0, /* had to disable because it doesn't recognize DOM objects */
-      "no-unused-vars":[0, {
+
+      /* ESLint */
+      "no-inner-declarations": 0,
+      "no-prototype-builtins": 0,
+      "no-redeclare": 0,
+      "no-undef": 0, /* had to disable because it doesn't recognize DOM objects */
+      "no-unused-vars": [0, {
         "argsIgnorePattern": "^_",
         "varsIgnorePattern": "^_",
-        "caughtErrorsIgnorePattern": "^_"
+        "caughtErrorsIgnorePattern": "^_",
       }],
+
+      /* Stylistic (alphabetically) */
       "@stylistic/arrow-parens": [0, "always"],
       "@stylistic/arrow-spacing": [0, { "before": true, "after": true }],
-      "@stylistic/brace-style":[0, "1tbs", {"allowSingleLine":false}],
+      "@stylistic/brace-style": [0, "1tbs", {"allowSingleLine": false}],
       "@stylistic/comma-dangle": [1, "always-multiline"],
       "@stylistic/comma-spacing": [1, { "before": false, "after": true }],
       "@stylistic/comma-style": [1, "last"],
@@ -35,45 +39,31 @@ export default [
       "@stylistic/function-paren-newline": [0, "multiline"],
       "@stylistic/implicit-arrow-linebreak": [0, "beside"],
       "@stylistic/indent": [1, 2],
-      "@stylistic/indent-binary-ops":[0, 2],
-      "@stylistic/key-spacing":[1, {
-        "beforeColon":false,
-        "afterColon":true,
-        "mode":"strict"
-      }],
-      "@stylistic/keyword-spacing":[1, {
-        "before": true,
-        "after": true,
-        "overrides": {}
-      }],
-      "@stylistic/lines-around-comment":[0, {"beforeBlockComment": true} ],
-      "@stylistic/lines-between-class-members":[0,{
-        "enforce": "always",
-        "exceptAfterSingleLine": false,
-      }],
-      "@stylistic/max-len":[0,{
+      "@stylistic/indent-binary-ops": [0, 2],
+      "@stylistic/key-spacing": [1, { "beforeColon": false, "afterColon": true, "mode": "strict" }],
+      "@stylistic/keyword-spacing": [1, { "before": true, "after": true, "overrides": {} }],
+      "@stylistic/lines-around-comment": [0, {"beforeBlockComment": true} ],
+      "@stylistic/lines-between-class-members": [0, { "enforce": "always", "exceptAfterSingleLine": false }],
+      "@stylistic/max-len": [0, {
         "code": 120,
         "tabWidth": 2,
         "ignoreUrls": true,
         "ignoreStrings": true,
         "ignoreTemplateLiterals": true,
-        "ignoreRegExpLiterals": true
+        "ignoreRegExpLiterals": true,
       }],
-      "@stylistic/max-statements-per-line":[0, {"max":1} ],
-      "@stylistic/new-parens":[0, "always"],
-      "@stylistic/no-extra-semi":[1],
-      "@stylistic/no-floating-decimal":[2],
-      "@stylistic/no-multi-spaces":[1],
-      "@stylistic/no-tabs":[1],
-      "@stylistic/no-trailing-spaces":[1],
-      "@stylistic/object-curly-newline":[0, {"multiline":true} ],
-      "@stylistic/quotes":[0,
-        "double",
-        {"allowTemplateLiterals": true}
-      ],
-      "@stylistic/semi-style":[1, "last"],
-      "@stylistic/space-before-blocks":[1, "always"],
-      "@stylistic/spaced-comment":[1, "always"],
-    }
-  }
+      "@stylistic/max-statements-per-line": [0, {"max": 1} ],
+      "@stylistic/new-parens": [0, "always"],
+      "@stylistic/no-extra-semi": [1],
+      "@stylistic/no-floating-decimal": [2],
+      "@stylistic/no-multi-spaces": [1],
+      "@stylistic/no-tabs": [1],
+      "@stylistic/no-trailing-spaces": [1],
+      "@stylistic/object-curly-newline": [0, {"multiline": true}],
+      "@stylistic/quotes": [0, "double", {"allowTemplateLiterals": true}],
+      "@stylistic/semi-style": [1, "last"],
+      "@stylistic/space-before-blocks": [1, "always"],
+      "@stylistic/spaced-comment": [1, "always"],
+    },
+  },
 ]
