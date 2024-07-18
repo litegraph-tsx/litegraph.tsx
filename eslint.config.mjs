4e878e8693ec22a726369b80b7bbffce4c6bc22c
DrJKL <DrJKL0424@gmail.com>
2024-07-18 11:38:45 -0700

    Initial AirBnB lint commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,22 @@
 import js from "@eslint/js";
 import stylistic from '@stylistic/eslint-plugin';
 import stylisticJs from '@stylistic/eslint-plugin-js';
+import {FlatCompat} from '@eslint/eslintrc';
+
+import path from "path";
+import { fileURLToPath } from "url";
+
+// mimic CommonJS variables -- not needed if using CommonJS
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
 
 export default [
   js.configs.recommended,
+  ...compat.extends("airbnb"),
   {
     "name": "litegraph.tsx/standard",
     "languageOptions": {
