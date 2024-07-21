763faf0fa43af58ef496b6a0d49c27eb60fc2275
Daniel Lewis <daniel.lewis.ab@gmail.com>
2024-07-21 11:01:57 -0600

    JSDoc for Console.js

--- src/core/Console.js
+++ src/core/Console.js
@@ -1,7 +1,18 @@
-/*
- * usage: import { console as X }
- * X.level = 4;
- * X.log?.('Message');
+/**
+ * This modification defines a Proxy for console which masks logging methods based on a specified logging level.
+ * @Usage:
+ * import { console } from <here>; // overrides default console
+ * import { console as whatever } from <here> // doesn't
+ * console.log(thing);
+ *
+ * @typedef {Object} Console
+ * @property {number} level - The logging level (0-5) where:
+ *   0: Error messages only
+ *   1: Errors and warnings
+ *   2: Errors, warnings, and regular logs
+ *   3: Errors, warnings, regular logs, and info messages
+ *   4: Errors, warnings, regular logs, info messages, and debug messages
+ *   5: Errors, warnings, regular logs, info messages, debug messages, and verbose debug messages
  */
 export const console = new Proxy(globalThis.console, {
   set(target, property, value) {
